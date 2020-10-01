0e563741ce006f0a75bcb6b9aa96fbc89f9d75e2
Slawomir Jablonski <56278753+sjablo@users.noreply.github.com>
2020-10-01 18:29:46 +0200

    Change default mocked task number to 0

--- src/mocks/tasks.js
+++ src/mocks/tasks.js
@@ -1,4 +1,4 @@
-const generateTasks = (number = 3) => {
+const generateTasks = (number) => {
   const taskArray = [];
   for (let i = 1; i <= number; i++) {
     taskArray.push({
@@ -9,4 +9,4 @@ const generateTasks = (number = 3) => {
   return taskArray;
 };
 
-export default generateTasks(process.env.REACT_APP_TASK_NUMBER);
+export default generateTasks(process.env.REACT_APP_MOCKED_TASK_NUMBER);
