ca095280c94da4e2d94182454ae1f84245d6398e
Slawomir Jablonski <56278753+sjablo@users.noreply.github.com>
2020-10-19 04:10:10 +0200

    Fix getData error

--- src/utils/storage/getData.js
+++ src/utils/storage/getData.js
@@ -1,10 +1,17 @@
 import { tasks } from "mocks";
 
 // TODO add parameter to getData for choosing which mocked data to include
-const getData = (label) =>
-  JSON.parse(window.localStorage.getItem(label)) ||
-  process.env.NODE_ENV === "production"
-    ? []
-    : tasks;
+const getData = (label) => {
+  const initialData = process.env.NODE_ENV === "production" ? [] : tasks;
+  const parsedData = JSON.parse(window.localStorage.getItem(label));
+
+  // no record in local storage - produces null, return mocks or empty array
+  if (!parsedData) return initialData;
+  // has an empty record in local storage - produces empty array
+  // TODO add guard against empty objects
+  else if (!parsedData.length) return initialData;
+  // has record in local storage - parse it
+  else return parsedData;
+};
 
 export default getData;
